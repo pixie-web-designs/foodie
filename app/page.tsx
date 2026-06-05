85895a19f43cd39c0ffa6ef67c946ba3d727a68b
Dayna Braverman <daynabraverman@gmail.com>
2026-06-05 13:30:25 -0400

    Add sizing for main

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
+const sLand = {
+  default: "w-full h-auto",
+};
+
 export default function Home() {
-  return <main></main>;
+  return <main className={sLand.default}></main>;
 }
