1112ee61e63c16b7efd96fbd5af57a57b55db71d
Dayna Braverman <daynabraverman@gmail.com>
2026-06-05 12:09:31 -0400

    finish tutorial

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,20 +3,31 @@ import React from "react";
 interface User {
   id: number;
   name: string;
+  email: string;
 }
 
 const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", { cache: "no-store" });
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const users: User[] = await res.json();
   return (
     <>
       <h1>Users</h1>
-      <p>{new Date().toLocaleTimeString()}</p>
-      <ul>
-        {users.map((u) => (
-          <li key={u.id}>{u.name}</li>
-        ))}
-      </ul>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((u) => (
+            <tr key={u.id}>
+              <td>{u.name}</td>
+              <td>{u.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
