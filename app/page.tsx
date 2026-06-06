90051376e7ec8d3940b41e15405006aab9e58fe6
Dayna Braverman <daynabraverman@gmail.com>
2026-06-06 15:25:56 -0400

    Add backend connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import React from "react";
 
 const sLand = {
   default: "w-full h-auto",
 };
 
-export default function Home() {
-  return <main className={sLand.default}></main>;
+interface Mes {
+  status: string;
+  message: string;
 }
+
+const Home = async () => {
+  const res = await fetch(process.env.API_URL);
+  const mes: Mes = await res.json();
+  return (
+    <main className={sLand.default}>
+      <p>{mes.message}</p>
+    </main>
+  );
+};
+
+export default Home;
