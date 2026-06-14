3287de1a7b9f500ecf1a8ce54ed4b45106558e64
Dayna Braverman <daynabraverman@gmail.com>
2026-06-14 17:37:11 -0400

    Update requirements and models

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr
 
+# User data
 class RegisterRequest(BaseModel):
   name: str
   email: EmailStr
@@ -12,3 +13,34 @@ class User(BaseModel):
   password: str
   verified: bool = False
   verification_token: str | None = None
+
+# Restaurant data
+class Restaurant(BaseModel):
+  id: int
+  name: str
+  address: str
+  img: str
+  alt: str
+  status: list[str]
+  tags: list[str]
+
+class Menu(BaseModel):
+  id: int
+  restaurantId: int
+  desc: str
+
+class Category(BaseModel):
+  id: int
+  menuId: int
+  name: str
+  displayOrder: int
+
+class Item(BaseModel):
+  id: int
+  categoryId: int
+  name: str
+  desc: str
+  price: float
+  hasAlcohol: bool
+  dietaryTags: list[str]
+  allergens: list[str]
